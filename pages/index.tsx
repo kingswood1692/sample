cd93cfae556b1f0c79d8a94c15aed3f367e06265
Sheerwood John Caday <sheerwoodjohncaday@sheerwoods-MacBook-Pro.local>
2021-10-12 23:07:05 +0800

    init

--- pages/index.tsx
+++ pages/index.tsx
@@ -71,21 +71,23 @@ const Home: NextPage = (props) => {
 };
 
 export async function getServerSideProps(ctx) {
-  // get the current environment
-  let dev = process.env.NODE_ENV !== "production";
-  let { DEV_URL, PROD_URL } = process.env;
+  if (ctx.req) {
+    // get the current environment
+    let dev = process.env.NODE_ENV !== "production";
+    let { DEV_URL, PROD_URL } = process.env;
 
-  // request posts from api
-  let response = await fetch(`${dev ? DEV_URL : PROD_URL}/api/hello`);
-  console.log(response)
-  // extract the data
-  let data = await response.json();
+    // request posts from api
+    let response = await fetch(`${dev ? DEV_URL : PROD_URL}/api/hello`);
+    console.log(response);
+    // extract the data
+    let data = await response.json();
 
-  return {
-    props: {
-      sites: data,
-    },
-  };
+    return {
+      props: {
+        sites: data,
+      },
+    };
+  }
 }
 
 export default Home;
