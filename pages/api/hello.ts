3abff66dd3dfce0825be0aee362119fe524ef3f3
Sheerwood John Caday <sheerwoodjohncaday@sheerwoods-MacBook-Pro.local>
2021-10-12 20:39:50 +0800

    init

--- pages/api/hello.ts
+++ pages/api/hello.ts
@@ -1,13 +1,33 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import type { NextApiRequest, NextApiResponse } from 'next'
 
+import dbConnect from "src/lib/dbConnect";
+import Sites from "src/models/Sites";
+
 type Data = {
   name: string
 }
 
-export default function handler(
+export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse<Data>
 ) {
-  res.status(200).json({ name: 'John Doe' })
+  // res.status(200).json({ name: 'John Doe' })
+  const { method } = req;
+
+  await dbConnect();
+
+  switch (method) {
+    case "GET":
+      try {
+        const getSites = await Sites.find({}).lean();
+        res.status(200).json({ success: true, data: getSites });
+      } catch (error) {
+        res.status(400).json({ success: false });
+      }
+      break;
+    default:
+      res.status(400).json({ success: false });
+      break;
+  }
 }
